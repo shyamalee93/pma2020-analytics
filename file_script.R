1b34b021eb8b66164b44523b5e46a52a0635dc4f
shyamalee93 <shyamalee93@gmail.com>
2016-10-17 13:22:11 -0400

    More edits
    Replaced 0 with NA for JPEG filesize,
    Added similar ifelse for XML and log file sizes

--- file_script.R
+++ file_script.R
@@ -1,5 +1,5 @@
 ##Get the RScripts directory from user
-rscript_dir <- readline("Enter the Rscripts directory")
+rscript_dir <- readline("Enter the RC:\Users\Shyamalee\Desktop\PMA2020\PMA2020scripts directory")
 
 #Set working directory as the rscripts_dir
 setwd(rscript_dir)
@@ -15,16 +15,19 @@ fun <- function(path) {
   submission <- 'submission.xml'
   submission_path <- file.path(path, submission)
   xml_size <- file.info(submission_path)$size
+  xml_size<- ifelse(length(xml_size)== 0, NA, xml_size)
 
   log <- 'log.txt'
   log_path <- file.path(path, log)
   log_size <- file.info(log_path)$size
+  log_size<- ifelse(length(log_size)== 0, NA, log_size)
 
   allfiles <- list.files(path)
   jpeg <- allfiles[grep("*.\\.[jJ][pP][eE]?[gG]$", allfiles)]
   jpeg_path <- file.path(path, jpeg)
   jpeg_size <- file.info(jpeg_path)$size
-  jpeg_size<- ifelse(length(jpeg_size)== 0, 0, jpeg_size)
+  jpeg_size<- ifelse(length(jpeg_size)== 0, NA, jpeg_size)
+  #Add a condition - if there are over one images?
 
   df <- data.frame(path=path, xml=xml_size, log=log_size, jpeg = jpeg_size)
   df
